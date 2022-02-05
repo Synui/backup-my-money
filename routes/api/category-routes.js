21d1930347c3ad44fa624a6dbfe41e2d86e70cf6
Synui <jonathon.schoenholz@yahoo.com>
2022-02-05 12:31:07 -0500

    POST routes working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -57,21 +57,29 @@ router.get('/:id', (req, res) => {
       }
     ]
   })
-  .then(dbCategoryData => {
-    if (!dbCategoryData) {
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
         res.status(404).json({ message: 'No post found with this id' });
         return;
-    }
-    res.json(dbCategoryData);
-})
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  })
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
