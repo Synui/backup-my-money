a21b81baac2e1f12775a10f152f8e8bed258178b
Synui <jonathon.schoenholz@yahoo.com>
2022-02-04 22:58:26 -0500

    GET all routes working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,30 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    order: [['category_name', 'ASC']],
+    attributes: [
+      'id',
+      'category_name',
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock',
+          'category_id'
+        ]
+      }
+    ]
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
